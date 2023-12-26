8bb69cc3eb0c49d6f72a1005831387afc0ac6cda
SakharovaKsu <nagis38@gmail.com>
2023-12-26 12:51:20 +0300

    Added react-hook-form and zod, form validation

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,28 +1,87 @@
-import { NavLink } from 'react-router-dom'
+import { FC } from 'react'
+import { useForm } from 'react-hook-form'
 
+import { zodResolver } from '@hookform/resolvers/zod'
+import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
+import { z } from 'zod'
 
 import s from './form.module.scss'
 
-export const Form = () => {
+type FormValues = z.infer<typeof formSchema>
+type Props = {
+  disabled?: boolean
+}
+
+const formSchema = z.object({
+  address: z.string().min(3),
+  email: z.string().trim().email(),
+  telephone: z
+    .string()
+    .trim()
+    .refine(value => /^[78]\d{10}$/.test(value)),
+  userName: z.string().min(3),
+})
+
+export const Form: FC<Props> = ({}) => {
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<FormValues>({
+    defaultValues: {
+      address: '',
+      email: '',
+      telephone: '',
+      userName: '',
+    },
+    resolver: zodResolver(formSchema),
+  })
+
+  const onSubmit = (data: FormValues) => {
+    // console.log(data)
+  }
+
   return (
-    <div className={s.container}>
+    <form className={s.container} onSubmit={handleSubmit(onSubmit)}>
       <TextField
+        {...register('userName')}
         color={'warning'}
-        id={'filled-basic'}
+        error={!!errors.userName?.message}
+        helperText={errors.userName?.message}
+        id={'userName'}
         label={'Ваша Фамилия, Имя'}
         variant={'filled'}
       />
       <TextField
+        {...register('address')}
         color={'warning'}
-        id={'filled-basic'}
-        label={'Куда нужно отправить'}
+        error={!!errors.address?.message}
+        helperText={errors.address?.message}
+        id={'address'}
+        label={'Куда нужно отправить?'}
         variant={'filled'}
       />
-      <TextField color={'warning'} id={'filled-basic'} label={'Ваш телефон'} variant={'filled'} />
-      <NavLink className={s.link} to={'/'}>
+      <TextField
+        {...register('telephone')}
+        color={'warning'}
+        error={!!errors.telephone?.message}
+        helperText={errors.telephone?.message}
+        id={'telephone'}
+        label={'+7(000)000-00-00'}
+        variant={'filled'}
+      />
+      <TextField
+        {...register('email')}
+        color={'warning'}
+        error={!!errors.email?.message}
+        id={'email'}
+        label={'Email'}
+        variant={'filled'}
+      />
+      <Button color={'warning'} fullWidth type={'submit'} variant={'contained'}>
         Оформить
-      </NavLink>
-    </div>
+      </Button>
+    </form>
   )
 }
